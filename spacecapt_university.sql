060cf4a125ec1f5aa2c6828a510fa2378a71d0a3
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-08 00:04:41 +0700

    pertama

--- spacecapt_university.sql
+++ spacecapt_university.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2021 at 11:42 AM
+-- Generation Time: Dec 07, 2021 at 06:04 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -39,9 +39,21 @@ CREATE TABLE `datamahasiswa` (
 --
 
 INSERT INTO `datamahasiswa` (`NPM`, `Nama`, `Kota`, `id_Jurusan`) VALUES
+(19024010014, 'Alifah Rafidah Putri', 'Jakarta', 22),
+(19024010065, 'Fairus Hanani', 'Palembang', 22),
+(19024010098, 'Aura Brhitania', 'Bandung', 22),
+(19025010011, 'Laras Setyowati', 'Purwokerto', 21),
+(19025010014, 'Anisah Qurrotu Aini', 'Nabire', 21),
+(19025010117, 'Barna Deta Putra', 'Purbalingga', 21),
 (19081010046, 'Fahri Izzuddin Zulkarnaen', 'Surabaya', 11),
-(19081010072, 'ishak', 'Sidoarjo', 12),
-(19081010185, 'Deva', 'Bekasi', 13);
+(19081010072, 'Ishak Febrianto', 'Solo', 11),
+(19081010185, 'Deva Dwi Satrio', 'Bekasi', 11),
+(19082010039, 'Fahri Arya Kharisma', 'Sidoarjo', 12),
+(19082010082, 'Yusman Zulfandra', 'Surabaya', 12),
+(19082010114, 'Achmad Chusen', 'Pamekasan', 12),
+(19083010006, 'Dendy Arizki', 'Malang', 13),
+(19083010051, 'Sandria Amelia Putri', 'Bangil', 13),
+(19083010074, 'Luqna Aziziyah', 'Lumajang', 13);
 
 -- --------------------------------------------------------
 
@@ -60,8 +72,7 @@ CREATE TABLE `fakultas` (
 
 INSERT INTO `fakultas` (`id_Fakultas`, `nama_Fakultas`) VALUES
 (1, 'Fakultas Ilmu Komputer'),
-(2, 'Fakultas Pertanian'),
-(3, 'Fakultas Teknik');
+(2, 'Fakultas Pertanian');
 
 -- --------------------------------------------------------
 
@@ -84,10 +95,26 @@ INSERT INTO `jurusan` (`id_Fakultas`, `id_Jurusan`, `nama_Jurusan`) VALUES
 (1, 12, 'Sistem Informasi'),
 (1, 13, 'Sains Data'),
 (2, 21, 'Agroteknologi'),
-(2, 22, 'Agribisnis'),
-(3, 31, 'Teknik Kimia'),
-(3, 32, 'Teknik Industri'),
-(3, 33, 'Teknologi Pangan');
+(2, 22, 'Agribisnis');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `Username` varchar(25) NOT NULL,
+  `Password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`Username`, `Password`) VALUES
+('fhr', '$2y$10$KQyieIaJgsDqsMr949wW6.jItEckIhkAT4S86VDq2T1Mvpzo5D6U2'),
+('mimina', '$2y$10$v/Pp.WYwV6YaRwQogjH8W.vm/LbLoLGA91m3k7yp4QcwOv07pP5RO');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +139,12 @@ ALTER TABLE `fakultas`
 ALTER TABLE `jurusan`
   ADD PRIMARY KEY (`id_Jurusan`),
   ADD KEY `id_Fakultas` (`id_Fakultas`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Username`);
 
 --
 -- Constraints for dumped tables
